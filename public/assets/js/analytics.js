e519ee83595bdee56b440e5afde90d76346afffd
hongping <your.email@exampl>
2026-04-23 18:30:04 +0800

    chore: configure GA4 measurement ID G-HX64K7DBXC

---
+++ public/assets/js/analytics.js
@@ -0,0 +1,25 @@
+(function initGa4() {
+  var measurementId = "G-HX64K7DBXC";
+
+  if (!measurementId || measurementId === "G-XXXXXXXXXX") {
+    console.warn("[GA4] Measurement ID is not set. Update /public/assets/js/analytics.js");
+    return;
+  }
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    window.dataLayer.push(arguments);
+  }
+
+  window.gtag = window.gtag || gtag;
+  gtag("js", new Date());
+  gtag("config", measurementId, {
+    send_page_view: true,
+    anonymize_ip: true
+  });
+
+  var gaScript = document.createElement("script");
+  gaScript.async = true;
+  gaScript.src = "https://www.googletagmanager.com/gtag/js?id=" + encodeURIComponent(measurementId);
+  document.head.appendChild(gaScript);
+})();
